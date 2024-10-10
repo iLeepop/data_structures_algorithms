d13fcf3d9e271933816e0d66b0b34c23877d1fcb
ileepop <zihao02325@gmail.com>
2024-10-10 20:15:14 +0800

    sync/1010

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
-// 插值查找
 mod way;
+mod data;
+mod utils;
 
 use data::*;
 use utils::*;
 use way::*;
 
 fn main() {
-    let mut stack = Stack::new();
-    stack.push(1);
 
-    par_checker("(23*((a+b)+(b+a)/45))%4");
-
-    let postfix = infix_to_postfix("(A+B)*C-D/E");
 }
