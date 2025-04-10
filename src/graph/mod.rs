84d398c092fd52b4a2a6cdaad3328f633a170d79
ileepop <zihao02325@gmail.com>
2025-04-10 17:47:34 +0800

    0410/word_lader

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,5 +1,6 @@
 mod graph_matrix;
 mod graph_adjlist;
 mod word_lader;
+mod bfs;
 
 pub use graph_adjlist::*;
--- src/graph/word_lader.rs
+++ src/graph/word_lader.rs
@@ -1,40 +1,129 @@
-use std::collections::HashMap;
-
 /// word lader
 
-use crate::graph::graph_adjlist::Graph;
+use std::collections::HashMap;
+use std::hash::Hash;
 
-use super::Vertex;
 
-fn build_word_graph(words: &[String]) -> Graph<String> {
-    let mut d: HashMap<String, Vec<String>> = HashMap::new();
-    for word in words {
-        for i in 0..word.len() {
-            let bucket = (word[..i].to_string() + "_")
-                                    + &word[i + 1..];
-            let wd = word.to_string();
-            if d.contains_key(&bucket) {
-                d.get_mut(&bucket).unwrap().push(wd);
-            } else {
-                d.insert(bucket, vec![wd]);
-            }
+#[derive(Clone, Debug, PartialEq)]
+enum Color {
+    White,
+    Gray,
+    Black,
+}
+
+
+#[derive(Debug, Clone)]
+struct Vertex<T> {
+    color: Color,
+    distance: u32,
+    key: T,
+    neighbors: Vec<(T, u32)>,
+}
+
+
+impl<T: Clone + PartialEq> Vertex<T> {
+    fn new(key: T) -> Self {
+        Self {
+            color: Color::White, 
+            distance: 0,
+            key,
+            neighbors: Vec::new(),
         }
     }
 
-    let mut g = Graph::new();
-    for bucket in d.keys() {
-        for wd1 in &d[bucket] {
-            for wd2 in &d[bucket] {
-                if wd1 != wd2 {
-                    g.add_edge(wd1, wd2, 1);
+    fn add_neighbor(&mut self, nbr: T, wt: u32) {
+        self.neighbors.push((nbr, wt));
+    }
+
+    fn get_neighbors(&self) -> Vec<&T> {
+        let mut neighbors = Vec::new();
+        for (nbr, _wt) in self.neighbors.iter() {
+            neighbors.push(nbr);
+        }
+        neighbors
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Graph<T> {
+    vertnums: u32,
+    edgenums: u32,
+    vertices: HashMap<T, Vertex<T>>,
+}
+
+impl<T: Hash + Eq + PartialEq + Clone> Graph<T> {
+    fn new() -> Self {
+        Self {
+            vertnums: 0,
+            edgenums: 0,
+            vertices: HashMap::<T, Vertex<T>>::new(),
+        }
+    }
+
+    fn contains(&self, key: &T) -> bool {
+        for (nbr, _vertex) in self.vertices.iter() {
+            if nbr == key {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn add_vertex(&mut self, key: &T) -> Option<Vertex<T>> {
+        let vertex = Vertex::new(key.clone());
+        self.vertnums += 1;
+        self.vertices.insert(key.clone(), vertex)
+    }
+
+    fn add_edge(&mut self, from: &T, to: &T, wt: u32) {
+        if !self.contains(from) {
+            let _fvert = self.add_vertex(from);
+        }
+        if !self.contains(to) {
+            let _tvert = self.add_vertex(to);
+        }
+
+        self.edgenums += 1;
+        self.vertices
+                .get_mut(from)
+                .unwrap()
+                .add_neighbor(to.clone(), wt);
+    }
+}
+
+
+// 构建图
+fn build_word_graph(words: Vec<&str>) -> Graph<String> {
+    let mut hmap: HashMap<String, Vec<String>> = HashMap::new();
+
+    for word in words {
+        for i in 0..word.len() {
+            let pattn = word[..i].to_string() + "_" + &word[i + 1..];
+            if hmap.contains_key(&pattn) {
+                hmap.get_mut(&pattn)
+                    .unwrap()
+                    .push(word.to_string());
+            } else {
+                hmap.insert(pattn, vec![word.to_string()]);
+            }
+        }
+    }
+    let mut word_graph = Graph::new();
+        for word in hmap.keys() {
+            for w1 in &hmap[word] {
+                for w2 in &hmap[word] {
+                    if w1 != w2 {
+                        word_graph.add_edge(w1, w2, 1);
+                    }
                 }
             }
         }
-    }
-
-    g
+    word_graph
 }
 
-fn word_lader(mut g: Graph<String>, mut start: Vertex<String>, end: Vertex<String>, len: usize) -> u32 {
+
+// 字梯图 - 广度优先搜索
+fn word_ladder() {
     
 }
+
